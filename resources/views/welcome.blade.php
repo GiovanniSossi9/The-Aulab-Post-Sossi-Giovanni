f645fd051d31fcd1cb9468cd00d30c7c89a286cd
GiovanniSossi9 <giovannisossi9@gmail.com>
2024-02-02 18:36:19 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,12 @@
-<h1>the aulab post</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>The Aulab Post</title>
+</head>
+<body>
+    <h1>The Aulab Post</h1>
+</body>
+</html>
